5b9cf3485f6f9cdab69b28fa8394f9ba23a7fe53
leonberkemeier <leonberkemeier@gmail.com>
2024-09-23 20:59:27 +0200

    filterWorks

--- finance/tracker/urls.py
+++ finance/tracker/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('edit_expense/<str:pk>/', views.editexpense, name='edit_expense'),
     path('delete_expense/<str:pk>/', views.deleteexpense, name='delete_expense'),
     path('list', views.list, name='list'),
+
+    
 ]
--- finance/tracker/views.py
+++ finance/tracker/views.py
@@ -2,20 +2,46 @@ from django.shortcuts import render, redirect
 
 from .forms import ExpensesForm
 from django.contrib import messages
-from datetime import date
+from datetime import date,datetime
+
 from .models import *
 
+from django.core import serializers
+from django.http import JsonResponse
+import sys
+
+import json
 # Create your views here.
 
 def home(request):
+    
     return render(request, "index.html")
 
 def list(request):
 
-    expenses = ExpensesItem.objects.all()
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    expenses = ExpensesItem.objects
     
+    year = request.GET.get('year', str(current_year))
+    month = request.GET.get('month', str(current_month))
+
+    if year is not None and year != '':
+        expenses = expenses.filter(date__year=year)
+    
+    if month is not None and month != '':
+        expenses = expenses.filter(date__month =month)
+    
+    expenses = expenses.all()
+
+    
+
+    data = serializers.serialize('json', expenses)
     context = {
-        'expenses' : expenses
+        'expenses' : expenses,
+        'data':data,
+        'month':month,
+        'year':year
     }
     return render(request, 'list.html', context)
 
